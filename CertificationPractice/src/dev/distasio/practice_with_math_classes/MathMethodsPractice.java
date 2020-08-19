5bf93961191ab70e8d3de9844b834074b1c0f678
Christopher DiStasio <cadistasio@gmail.com>
2020-08-19 18:27:23 -0500

    Adds round(), ceil() and floor() to MainMethodsPractice

--- CertificationPractice/src/dev/distasio/practice_with_math_classes/MathMethodsPractice.java
+++ CertificationPractice/src/dev/distasio/practice_with_math_classes/MathMethodsPractice.java
@@ -3,9 +3,7 @@ package dev.distasio.practice_with_math_classes;
 public class MathMethodsPractice {
 	
 	public static void main (String args[]) {
-		
-        // Random random = new Random();
-        
+		        
         double random = (double)(Math.random() * 100);
         
         int roundedRandom = (int)Math.round(random);
@@ -13,6 +11,21 @@ public class MathMethodsPractice {
         System.out.println(random);
 
         System.out.println(roundedRandom);
+        
+        
+        double d = 4.5;
+        
+        int round = (int) Math.round(d);
+        
+        int ceil = (int) Math.ceil(d);
+
+        int floor = (int) Math.floor(d);
+        
+        System.out.println(round);
+        
+        System.out.println(ceil);
+        
+        System.out.println(floor);
 
 	}
 
