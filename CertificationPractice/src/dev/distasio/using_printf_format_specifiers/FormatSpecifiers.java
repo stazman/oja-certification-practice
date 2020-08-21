0205ec8dde0bea1c2deb58ce1a719025af4ea63d
Christopher DiStasio <cadistasio@gmail.com>
2020-08-20 20:34:15 -0500

    Completes complicated string with %s, %d

--- CertificationPractice/src/dev/distasio/using_printf_format_specifiers/FormatSpecifiers.java
+++ CertificationPractice/src/dev/distasio/using_printf_format_specifiers/FormatSpecifiers.java
@@ -9,8 +9,12 @@ public class FormatSpecifiers {
 		
 		System.out.printf("%s%s%n", "Hello, ", "John"); //Hello, John
 		
-		System.out.printf("%s %s", "Hello,", "John"); //Hello, John
-
+		System.out.printf("%s %s%n", "Hello,", "John"); //Hello, John
+		
+		System.out.printf("%d%n", 3);
+		
+		System.out.printf("%s%s%d%d%d%s%d%d%d%s%d%d%d%d%s", "My phone number is ", "(", 5,5,5, ")", 5,5,5, "-", 1,2,3,4, ".");
+		
 	}
 
 }
