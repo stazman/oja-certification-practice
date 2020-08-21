3e6ac938d0ef43792d2f1ac8b6b4cca5a842ddd6
Christopher DiStasio <cadistasio@gmail.com>
2020-08-20 20:05:14 -0500

    Adds FormatSpecifiers; completes function with %s format specifier

---
+++ CertificationPractice/src/dev/distasio/using_printf_format_specifiers/FormatSpecifiers.java
@@ -0,0 +1,15 @@
+package dev.distasio.using_printf_format_specifiers;
+
+public class FormatSpecifiers {
+
+	public static void main(String[] args) {
+
+
+		System.out.printf("%s%s", "Hello, ", "John" + "\n"); //Hello, John
+		
+		
+		System.out.printf("%s %s", "Hello,", "John"); //Hello, John
+
+	}
+
+}
