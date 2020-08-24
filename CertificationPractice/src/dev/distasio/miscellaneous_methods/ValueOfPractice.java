c3c60b3cf474d542dbb370a18ab4abccc6e691ae
Christopher DiStasio <cadistasio@gmail.com>
2020-08-24 12:14:15 -0500

    Adds, completes ValueOfPractice

---
+++ CertificationPractice/src/dev/distasio/miscellaneous_methods/ValueOfPractice.java
@@ -0,0 +1,16 @@
+package dev.distasio.miscellaneous_methods;
+
+public class ValueOfPractice {
+
+	public static void main(String[] args) {
+
+		String s = "1234";
+		
+		int i = Integer.valueOf(s);
+		
+		
+		System.out.println(i);
+		
+	}
+
+}
