384dcf05dc119ddfe83e0a7edf7560403b67245f
Christopher DiStasio <cadistasio@gmail.com>
2020-09-10 21:11:07 -0500

    Adds, completes LoopTestClass

--- CertificationPractice/src/dev/distasio/miscellaneous_methods/LoopTestClass.java
+++ CertificationPractice/src/dev/distasio/miscellaneous_methods/LoopTestClass.java
@@ -87,5 +87,17 @@ public class LoopTestClass {
 		  
 		  System.out.println(t + u);
 	   }
+	   
+	   
+	   int count = 0, sum = 0;
+	   do
+	   {
+	      if(count % 3 == 0) continue;
+	      sum+=count;
+	   }
+	   while(count++ < 11);
+	   System.out.println(sum);
+	   
+	   
    }
 }
