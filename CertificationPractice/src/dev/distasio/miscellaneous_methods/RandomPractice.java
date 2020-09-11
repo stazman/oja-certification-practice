7ce6be5bf246c2edc5ec4a8c538ce16dd716b31f
Christopher DiStasio <cadistasio@gmail.com>
2020-09-10 20:51:16 -0500

    Adds practice with seeding and java.util.Random, java.lang.Math methods

--- CertificationPractice/src/dev/distasio/miscellaneous_methods/RandomPractice.java
+++ CertificationPractice/src/dev/distasio/miscellaneous_methods/RandomPractice.java
@@ -32,6 +32,57 @@ public class RandomPractice {
         
         System.out.println(e);
           
+        
+        Random r4 = new Random();
+        
+        r4.setSeed(12);
+        
+        double f = r4.nextDouble();
+        
+        System.out.println(f);
+        
+        
+        Random r5 = new Random(12);
+        
+        r5.setSeed(12);
+        
+        double g = r4.nextDouble();
+        
+        System.out.println(g);
+        
+        
+        Random r6 = new Random();
+        
+        int i6 = r6.nextInt(10);
+        
+        System.out.println(i6);
+        
+        //Between 1 and 10 different each time
+        
+        
+        Random r7 = new Random(10);
+        
+        int i7 = r7.nextInt();
+        
+        System.out.println(i7);
+        
+        //Between 1 and 10 same each time
+
+        
+        Random r8 = new Random(10);
+        
+//        r8.setSeed(10); [does the same as passing 10 to new Random()]
+    
+        
+        int i8 = (int) r8.nextInt() * 10;
+        
+        System.out.println(i8);
+        
+        
+        int i9 = 10;
+        
+        System.out.println(Math.round(Math.random()*i9));
+        
 	}
 
 }
